140e0ead892a2dd874dfce321d2e237d3393271a
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-01-06 17:57:45 +0500

    childSection4 created

---
+++ src/components/childSection4.jsx
@@ -0,0 +1,82 @@
+import "./childSection4.css";
+
+export default function ChildSection4() {
+  return (
+    <>
+      <div className="main">
+        <p>Solutions for All</p>
+        <h5>Take ownership of your digital experience.</h5>
+        <div className="card__section">
+          <ul>
+            <li>
+              <img src="" alt="" />
+              <h3>Web Agencies</h3>
+              <p>
+                Spin up new sites and client campaigns faster than ever before.
+                Launch dynamic websites that leverage the power of
+                ApostropheCMS, our open source Node.js CMS. Utilize your team`s
+                Javascript expertise and pattern library to launch websites
+                faster and make your projects more profitable.
+              </p>
+              <p>Deliever ahead of schedule</p>
+            </li>
+            <li>
+              <img src="" alt="" />
+              <h3>SaaS Companies</h3>
+              <p>
+                Be the leading digital platform in your vertical, without any
+                limitations. Experience a white glove approach to white label
+                websites with Apostrophe Assembly. Quickly provide websites to
+                your customers with your tech built in with just a few clicks.
+              </p>
+              <p>More Assembly features</p>
+            </li>
+            <li>
+              <img src="" alt="" />
+              <h3>Higher Education</h3>
+              <p>
+                Operate in today`s modern technology with minds of the future.
+                You have hundreds of department, institute, center and lab
+                websites to manage. Spin up new websites with your brand built
+                in and easily onboard faculty and students to the digital
+                experience.
+              </p>
+              <p>More about Higher Ed features</p>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <img src="" alt="" />
+              <h3>Enterprise</h3>
+              <p>
+                Achieve digital success by managing everything in one place.
+                Edit all of your websites across multiple languages and brands
+                with one code base and one dashboard to oversee it all. With
+                Apostrophe Assembly you get to work with great tools while being
+                supported by a dedicated team.
+              </p>
+              <p>Michelin powered by Apostrophe</p>
+            </li>
+            <li>
+              <img src="" alt="" />
+              <h3>Hospitality</h3>
+              <p>
+                Take control of your customer`s journey before they even step
+                foot into your restaurant or hotel. Integrate with booking and
+                reservation engines and control content across several regions
+                and locales. Apostrophe puts power in the hands of developers
+                and editors to begin your brand`s promise with the very first
+                click.
+              </p>
+              <p>Read Kimpton`s story</p>
+            </li>
+            <li>
+              <h3>View Plans and Pricing</h3>
+              <p>Find the plan that`s right for me</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
