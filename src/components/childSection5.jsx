ea9dfc0b4fe0a2ac4aa9568eb452c91f60711d6a
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-01-09 21:47:29 +0500

    Carousel in process

--- src/components/childSection5.jsx
+++ src/components/childSection5.jsx
@@ -1,10 +1,14 @@
 import Card from "./Card";
+import "./childSection5.css";
 import Carousel from "react-elastic-carousel";
 
 export default function ChildSection5() {
   return (
-    <div>
-      <Carousel showArrows={false}>
+    <div className="Carousel-style">
+      <Carousel showArrows={false} itemsToShow={1}>
+        <Card />
+        <Card />
+        <Card />
         <Card />
       </Carousel>
     </div>
